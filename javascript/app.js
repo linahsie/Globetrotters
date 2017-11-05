75c70196457d63d1e36f06732674fadd92edd6cb
Wenzheng <whu14@illinois.edu>
2017-11-05 17:02:16 -0600

    Add Search. Separate javascript .

---
+++ javascript/app.js
@@ -0,0 +1,6 @@
+function open_sidebar() {
+    document.getElementById("sidebar").style.display = "block";
+}
+function close_sidebar() {
+    document.getElementById("sidebar").style.display = "none";
+}
