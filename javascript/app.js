77f6aaf3224534bcc79b1fa466cc811eca9c1329
whu14 <whu14@illinois.edu>
2017-11-16 16:58:26 -0600

    Solve bugs on dynamic post display.

--- javascript/app.js
+++ javascript/app.js
@@ -91,7 +91,7 @@ $( document ).ready(function () {
         tagName: 'awesome',
         accessToken: '6419152996.166bd80.3f5e1392db904047832daf97129569e4',
         userId:'6419152996',
-        template: '<div class="post"><p id ={{id}} class="owner"></p> <div onclick="changeStarColor(this)" class = "like_container"> <p id="star" > &#9733;</p> <p id="add" >Add to favorite</p> </div> <div class="post_content" onclick="open_post_modal(this)"> <div class="post_info"> <p>{{caption}}</p> <span class="likes"><i class="icon ion-heart"></i> {{likes}}</span><span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span> </div> <div> <img id ="post_image" src="{{image}}" alt="" class="img-responsive"> </div> </div> </div>',
+        template: '<div class="post"><p id ={{id}} class="owner"></p> <div onclick="changeStarColor(this)" class = "like_container"> <p id="star" > &#9733;</p> <p id="add" >Add to favorite</p> </div> <div class="post_content" onclick="open_post_modal(this)"> <div class="post_info"> <p>{{caption}}</p> <div id="comment_like"> <span class="likes"><i class="icon ion-heart"></i> {{likes}}</span><span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span></div> </div> <div> <img id ="post_image" src="{{image}}" alt="" class="img-responsive"> </div> </div> </div>',
         resolution: 'standard_resolution'
     });
     feed.run();
